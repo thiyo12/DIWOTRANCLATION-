6ed289eaa7ffdd319d4862be13acf371d92733d3
thiyo12 <66420586+thiyo12@users.noreply.github.com>
2026-09-25 00:21:44 +0530

    security: avoid persistent storage for customer access tokens

--- js/fillform.js
+++ js/fillform.js
@@ -353,7 +353,7 @@
           return;
         }
         SSX.saveLocal("ssx.lastDocRef", res.ref);
-        if (res.access_token) SSX.saveLocal("ssx.access." + res.ref, res.access_token);
+        if (res.access_token) SSX.saveSecret("ssx.access." + res.ref, res.access_token);
         window.location.href = "confirmation.html?ref=" + encodeURIComponent(res.ref) + "&kind=document" +
           (res.access_token ? "&token=" + encodeURIComponent(res.access_token) : "");
       }).catch(function (err) {
